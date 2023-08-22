4b0357b8a4e4ea15045ac10565ab27c751701c33
widua <mikolaj.widla@gmail.com>
2023-08-22 19:30:53 +0200

    Created base for project, and writed docker-compose with keycloak and postgresql

---
+++ src/main/java/me/widua/keycloakedgraphql/KeycloakedGraphQlApplication.java
@@ -0,0 +1,14 @@
+package me.widua.keycloakedgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakedGraphQlApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakedGraphQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/widua/keycloakedgraphql/KeycloakedGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.widua.keycloakedgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakedGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
